6f3ed0e106fd128c308d91ad8ee511787bf01f98
sagarika saha <sagarika.saha1@gmail.com>
2023-01-27 05:11:23 +0530

    adding stuff to throw conflict

---
+++ src/main/java/DemoClass2.java
@@ -0,0 +1,10 @@
+public class DemoClass2 {
+
+    public static void randomMethod(String[] args) {
+        System.out.println("main class in");
+    }
+
+    public void randomMethod2(){
+        
+    }
+}
